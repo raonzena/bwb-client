db2d09c9303e8a44b7f24e83de5554a62030ac1a
iaan <iaan@iaanui-MacBook-Air.local>
2019-05-16 10:14:11 +0900

    fixing map

---
+++ googleMapPractice/googlemap2/src/GoogleMaps/Map.js
@@ -0,0 +1,197 @@
+import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap, withScriptjs, InfoWindow, Marker } from "react-google-maps";
+import Geocode from "react-geocode";
+import Autocomplete from 'react-google-autocomplete';
+// AIzaSyDXQtk-kNAiN0LotPao1ZVKxg4D1--hm-8
+
+Geocode.setApiKey( "AIzaSyDXQtk-kNAiN0LotPao1ZVKxg4D1--hm-8" );
+Geocode.enableDebug();
+
+class Map extends Component{
+	constructor( props ){
+		super( props );
+		this.state = {
+      address: '',
+      placeID: '',
+			mapPosition: {
+				lat: this.props.center.lat,
+				lng: this.props.center.lng
+			},
+			markerPosition: {
+				lat: this.props.center.lat,
+				lng: this.props.center.lng
+			}
+		}
+	}
+	/**
+	 * Get the current address from the default map position and set those values in the state
+	 */
+	componentDidMount() {
+		Geocode.fromLatLng( this.state.mapPosition.lat , this.state.mapPosition.lng ).then(
+      
+			response => {
+				const address = response.results[0].formatted_address,
+          addressArray =  response.results[0].address_components
+          console.log(addressArray, 'addressArray')
+
+				this.setState( {
+					address: ( address ) ? address : '',
+				} )
+			},
+			error => {
+				console.error( error );
+      }
+      
+		);
+	};
+	
+	shouldComponentUpdate( nextProps, nextState ){
+		if (
+			this.state.markerPosition.lat !== this.props.center.lat ||
+			this.state.address !== nextState.address
+		) {
+			return true
+		} else if ( this.props.center.lat === nextProps.center.lat ){
+			return false
+		}
+	}
+
+	onChange = ( event ) => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
+	/**
+	 * This Event triggers when the marker window is closed
+	 *
+	 * @param event
+	 */
+	onInfoWindowClose = ( event ) => {
+
+	};
+
+	/**
+	 * When the marker is dragged you get the lat and long using the functions available from event object.
+	 * Use geocode to get the address, city, area and state from the lat and lng positions.
+	 * And then set those values in the state.
+	 *
+	 * @param event
+	 */
+	onMarkerDragEnd = ( event ) => {
+		let newLat = event.latLng.lat(),
+			newLng = event.latLng.lng();
+
+		Geocode.fromLatLng( newLat , newLng ).then(
+			response => {
+				const address = response.results[0].formatted_address,
+					addressArray =  response.results[0].address_components
+
+				this.setState( {
+					address: ( address ) ? address : ''
+					
+				} )
+			},
+			error => {
+				console.error(error);
+			}
+		);
+	};
+
+	/**
+	 * When the user types an address in the search box
+	 * @param place
+	 */
+	onPlaceSelected = ( place ) => {
+		console.log( 'plc', place );
+		const address = place.formatted_address,
+			addressArray =  place.address_components,
+			
+			latValue = place.geometry.location.lat(),
+			lngValue = place.geometry.location.lng();
+		// Set these values in the state.
+		this.setState({
+			address: ( address ) ? address : '',
+			
+			markerPosition: {
+				lat: latValue,
+				lng: lngValue
+			},
+			mapPosition: {
+				lat: latValue,
+				lng: lngValue
+			},
+		})
+	};
+
+
+	render(){
+		const AsyncMap = withScriptjs(
+			withGoogleMap(
+				props => (
+					<GoogleMap google={ this.props.google }
+					           defaultZoom={ this.props.zoom }
+					           defaultCenter={{ lat: this.state.mapPosition.lat, lng: this.state.mapPosition.lng }}
+					>
+						{/* InfoWindow on top of marker */}
+						<InfoWindow
+							onClose={this.onInfoWindowClose}
+							position={{ lat: ( this.state.markerPosition.lat + 0.0018 ), lng: this.state.markerPosition.lng }}
+						>
+							<div>
+								<span style={{ padding: 0, margin: 0 }}>{ this.state.address }</span>
+							</div>
+						</InfoWindow>
+						{/*Marker*/}
+						<Marker google={this.props.google}
+						        name={'Dolores park'}
+						        draggable={true}
+						        onDragEnd={ this.onMarkerDragEnd }
+						        position={{ lat: this.state.markerPosition.lat, lng: this.state.markerPosition.lng }}
+						/>
+						<Marker />
+						{/* For Auto complete Search Box */}
+						<Autocomplete
+							style={{
+								width: '100%',
+								height: '40px',
+								paddingLeft: '16px',
+								marginTop: '2px',
+								marginBottom: '100px'
+							}}
+							onPlaceSelected={ this.onPlaceSelected }
+							types={['(regions)']}
+						/>
+					</GoogleMap>
+				)
+			)
+		);
+		let map;
+		if( this.props.center.lat !== undefined ) {
+			map = <div>
+				<div>
+					
+					<div className="form-group">
+						<label htmlFor="">Address</label>
+						<input type="text" name="address" className="form-control" onChange={ this.onChange } readOnly="readOnly" value={ this.state.address } />
+					</div>
+				</div>
+
+				<AsyncMap
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDXQtk-kNAiN0LotPao1ZVKxg4D1--hm-8&libraries=places&types=restaurant"
+          // googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyDXQtk-kNAiN0LotPao1ZVKxg4D1--hm-8/place/findplacefromtext/output?parameters'
+					loadingElement={
+						<div style={{ height: `100%` }} />
+					}
+					containerElement={
+						<div style={{ height: this.props.height }} />
+					}
+					mapElement={
+						<div style={{ height: `100%` }} />
+					}
+				/>
+			</div>
+		} else {
+			map = <div style={{height: this.props.height}} />
+		}
+		return( map )
+	}
+}
+export default Map
