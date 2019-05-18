af3b107d06c1fd60b010d7c3663221d8752e8fe2
정재범 <jjb@jeongjaebeom-ui-MacBookPro.local>
2019-05-18 18:59:10 +0900

    fuck merge client-dev

--- src/Components/MeetingLists.js
+++ src/Components/MeetingLists.js
@@ -1,27 +1,90 @@
-import React from "react";
+import React, { Component } from "react";
 
-const MeetingLists = props => {
-  return (
-    <div id="meetingListContainer">
-      {props.meetingListToRender.map(aList => (
-        <button
-          className="meetingListBtn"
-          key={aList.meetingId}
-          index={aList.meetingId}
-          onClick={() => props.getMeetingDetail(aList.placeId, aList.meetingId)}
-        >
-          <div>
-            <strong>{props.reataurantname}</strong>
-          </div>
-          <div>
-            <strong>{aList.meetingName}</strong>
-          </div>
-          <div>{aList.meetingTime}</div>
-          <div>{aList.numberOfMembers + "/" + aList.limit}</div>
-        </button>
-      ))}
-    </div>
-  );
+const activationFilter = meetingListArr => {
+  let result = {};
+  let activeMeetings = [];
+  let inActiveMeetings = [];
+  meetingListArr.sort(function(a, b) {
+    return (
+      Number(a.meetingTime.slice(11, 13)) - Number(b.meetingTime.slice(11, 13))
+    );
+  });
+  meetingListArr.forEach(e => {
+    if (e.isActive) {
+      activeMeetings.push(e);
+    } else {
+      inActiveMeetings.push(e);
+    }
+  });
+  result.activeMeetings = activeMeetings;
+  result.inActiveMeetings = inActiveMeetings;
+  return result;
 };
+
+class MeetingLists extends Component {
+  state = {
+    meetingsInfos: []
+  };
+
+  init = () => {
+    var p = Promise.resolve(this.props.meetingsInfos);
+    p.then(meetingLists => {
+      this.setState({
+        meetingsInfos: meetingLists
+      });
+    });
+  };
+
+  componentDidMount = () => {
+    this.init();
+  };
+
+  render() {
+    console.log(this.state.meetingsInfos);
+    console.log(this.props);
+    return (
+      <div id="meetingListContainer">
+        {!this.state.meetingsInfos || this.state.meetingsInfos.length !== 0 ? (
+          <div>
+            <div id="activeMeetings">
+              {activationFilter(
+                this.state.meetingsInfos.result
+              ).activeMeetings.map(aList => (
+                <button
+                  className="meetingListBtn"
+                  key={aList.meetingId}
+                  index={aList.meetingId}
+                >
+                  <div>
+                    <strong>{aList.meetingName}</strong>
+                  </div>
+                  <div>{aList.meetingTime}</div>
+                  <div>{aList.numberOfMembers + "/" + aList.limit}</div>
+                </button>
+              ))}
+            </div>
+            <div id="activeMeetings">
+              {activationFilter(
+                this.state.meetingsInfos.result
+              ).inActiveMeetings.map(aList => (
+                <button
+                  className="meetingListBtn"
+                  key={aList.meetingId}
+                  index={aList.meetingId}
+                >
+                  <div>
+                    <strong>{aList.meetingName}</strong>
+                  </div>
+                  <div>{aList.meetingTime}</div>
+                  <div>{aList.numberOfMembers + "/" + aList.limit}</div>
+                </button>
+              ))}
+            </div>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
 
 export default MeetingLists;
