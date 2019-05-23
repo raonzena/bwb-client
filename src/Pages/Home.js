4c312e9d3caf05085be8f8bebf6d2322f8eb844e
iaan <iaan@iaanui-MacBook-Air.local>
2019-05-23 17:39:23 +0900

    forRenewForking

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import MainSearch from "./MainSearch";
 import MapHouse from "./MapHouse";
+import PracticeModal from "./PracticeModal";
 const Home = props => {
   console.log(props, 'props')
   return (
     <div className="Bodys">
       <div className="MainSearch">
+      <PracticeModal />
         <MainSearch handleSearch={props.handleSearch} handleClickHome={props.handleClickHome} />
       </div>
       <div className="MapHouse">
